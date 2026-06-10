9a276e74be70f17eeb0586ce7c4f1eb491c5d7a2
MarcusJongste <jongsteml@gmail.com>
2026-06-10 22:42:55 +0200

    setup creates more elaborate config. node setup to auto run/import

--- src/node.ts
+++ src/node.ts
@@ -3,17 +3,14 @@ import searchDir from "./getTestData/getTestDataBrowser";
 import * as path from 'path';
 import { Config } from "./types";
 
-const configAuto = path.resolve(process.cwd(), 'flowy.config.ts');
-
-export const flowyTest = {
-    unitTest: (config?: Config) => {
-        return Promise.resolve(config ?? import(configAuto))
-            .then((finalConf) => {
-                console.log(getTestFiles(finalConf, searchDir))
-            });    
-    }
-};
+const rootPath = path.resolve(__dirname, '..', '..', '..');
+const configAuto = path.resolve(rootPath, 'flowy.config.ts');
+const flowytest = Promise.resolve(import(configAuto))
+    .then((finalConf) => {
+        console.log(getTestFiles(finalConf, searchDir))
+    });  
 
 export {
-    flowyTest as default
+    flowytest as default,
+    type Config
 }
--- src/setup.ts
+++ src/setup.ts
@@ -1,19 +1,35 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
-import { type Config } from '../src/types';
 
-const defaultConfig: Config = {
+const configContent = `import { Config } from 'flowy-test';
+/*
+ flowy-test will gather all functions found in the directory.
+ it will also find all test files matching the testFilePattern.
+ and match each testFile function unittest to the function.
+ any function without unittest is removed
+ then flowy-test will run all unittests
+ after you can choose how to process the tests
+ 1. throw error
+ 2. have custom code to display ui
+ 3. ...
+*/
+const config: Config = {
+    // write here the directories to test 
     dirs: [],
+    // files to ignore
     ignore: [/index\.(ts|js)$/],
+    // file extension to check
     extensions: ['js', 'ts'],
+    // which files are tests
     testFilePattern: /test\.(ts|js)$/
 };
+export default config;`;
 
 const projectRoot = path.resolve(__dirname, '..', '..', '..');
 const configPath = path.resolve(projectRoot, 'flowytest.config.json');
 if (!fs.existsSync(configPath)) {
-    fs.writeFileSync(configPath, JSON.stringify(defaultConfig, null, 2));
+    fs.writeFileSync(configPath, JSON.stringify(configContent, null, 2));
     console.log('created flowytest.config.json');
 } else {
     console.log('Config already exists, skipping flowytest.config creation');
