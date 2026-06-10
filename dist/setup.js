2dd89a10b66f9cd3f396c1e2707b1ad1f49c9617
MarcusJongste <jongsteml@gmail.com>
2026-06-10 22:23:51 +0200

    another attempt at getting to root

--- dist/setup.js
+++ dist/setup.js
@@ -35,18 +35,20 @@ var __importStar = (this && this.__importStar) || (function () {
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs = __importStar(require("fs"));
 const path = __importStar(require("path"));
-const configPath = path.resolve(process.cwd(), 'flowytest.config.json');
-if (fs.existsSync(configPath)) {
-    console.log('Config already exists, skipping flowytest.config creation');
-    process.exit(0);
-}
 const defaultConfig = {
     dirs: [],
     ignore: [/index\.(ts|js)$/],
     extensions: ['js', 'ts'],
     testFilePattern: /test\.(ts|js)$/
 };
-fs.writeFileSync(configPath, JSON.stringify(defaultConfig, null, 2));
-console.log('created flowytest.config.json');
+const projectRoot = path.resolve(__dirname, '..', '..', '..');
+const configPath = path.resolve(projectRoot, 'flowytest.config.json');
+if (!fs.existsSync(configPath)) {
+    fs.writeFileSync(configPath, JSON.stringify(defaultConfig, null, 2));
+    console.log('created flowytest.config.json');
+}
+else {
+    console.log('Config already exists, skipping flowytest.config creation');
+}
 console.log('[flowy-test] Installed successfully.');
 //# sourceMappingURL=setup.js.map
--- src/setup.ts
+++ src/setup.ts
@@ -3,18 +3,20 @@ import * as path from 'path';
 
 import { type Config } from '../src/types';
 
-const configPath = path.resolve('flowytest.config.json');
-if (fs.existsSync(configPath)) {
-    console.log('Config already exists, skipping flowytest.config creation');
-    process.exit(0);
-}
 const defaultConfig: Config = {
     dirs: [],
     ignore: [/index\.(ts|js)$/],
     extensions: ['js', 'ts'],
-    testFilePattern:/test\.(ts|js)$/
+    testFilePattern: /test\.(ts|js)$/
 };
 
-fs.writeFileSync(configPath, JSON.stringify(defaultConfig, null, 2));
-console.log('created flowytest.config.json');
+const projectRoot = path.resolve(__dirname, '..', '..', '..');
+const configPath = path.resolve(projectRoot, 'flowytest.config.json');
+if (!fs.existsSync(configPath)) {
+    fs.writeFileSync(configPath, JSON.stringify(defaultConfig, null, 2));
+    console.log('created flowytest.config.json');
+} else {
+    console.log('Config already exists, skipping flowytest.config creation');
+}
+
 console.log('[flowy-test] Installed successfully.');
