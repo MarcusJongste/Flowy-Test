2c609e47851ff19d8f42eb1cb89a29c66038dbaf
MarcusJongste <jongsteml@gmail.com>
2026-06-13 22:12:54 +0200

    changes.

--- dist/node.js
+++ dist/node.js
@@ -38,7 +38,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = void 0;
 const getTestFiles_1 = __importDefault(require("./getTestData/getTestFiles"));
-const getTestDataBrowser_1 = __importDefault(require("./getTestData/getTestDataBrowser"));
+const getTestDataNode_1 = __importDefault(require("./getTestData/getTestDataNode"));
 const path = __importStar(require("path"));
 const url_1 = require("url");
 const rootPath = path.resolve(__dirname, '..');
@@ -46,7 +46,7 @@ const configAuto = path.resolve(rootPath, 'flowytest.config.js');
 console.log('starting node test');
 const flowytest = Promise.resolve(Promise.resolve(`${(0, url_1.pathToFileURL)(configAuto).href}`).then(s => __importStar(require(s))))
     .then((finalConf) => {
-    console.log((0, getTestFiles_1.default)(finalConf, getTestDataBrowser_1.default));
+    console.log((0, getTestFiles_1.default)(finalConf.default, getTestDataNode_1.default));
 });
 exports.default = flowytest;
 //# sourceMappingURL=node.js.map
