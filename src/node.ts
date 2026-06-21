59680e88152e174d86fc61ba480f7ecac094b1ea
MarcusJongste <jongsteml@gmail.com>
2026-06-21 21:50:03 +0200

    setup self test function to run node test on self

--- src/node.ts
+++ src/node.ts
@@ -1,14 +1,21 @@
 import getTestFiles from "./getTestData/getTestFiles";
-import searchDir from "./getTestData/getTestDataBrowser";
+import searchDir from "./getTestData/getTestDataNode";
 import * as path from 'path';
+import { pathToFileURL } from 'url';
 import { Config } from "./types";
+import runTests from "./runTests";
 
-const rootPath = path.resolve(__dirname, '..', '..', '..');
-const configAuto = path.resolve(rootPath, 'flowy.config.ts');
-const flowytest = Promise.resolve(import(configAuto))
+const rootPath = path.resolve(__dirname, '..');
+const configAuto = path.resolve(rootPath, 'flowytest.config.js');
+console.log('starting node test');
+const flowytest = Promise.resolve(import(pathToFileURL(configAuto).href))
     .then((finalConf) => {
-        console.log(getTestFiles(finalConf, searchDir))
-    });  
+        return getTestFiles(finalConf.default, searchDir)
+            .then((testResult) => {
+                return runTests(testResult);
+            })
+    });
+flowytest.then((testResults) => console.log(testResults));
 
 export {
     flowytest as default,
---
+++ src/test.node.ts
@@ -0,0 +1,6 @@
+import flowytest from 'flowy-test';
+
+flowytest
+    .then((testResult) => {
+        console.log('testing completed');
+    })
