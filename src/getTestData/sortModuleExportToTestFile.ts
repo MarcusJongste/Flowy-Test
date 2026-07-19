93205701d1168375ee6e608f5ea1a9a70fd76802
MarcusJongste <jongsteml@gmail.com>
2026-07-19 13:04:32 +0200

    troubleshooting

--- src/getTestData/sortModuleExportToTestFile.ts
+++ src/getTestData/sortModuleExportToTestFile.ts
@@ -37,7 +37,7 @@ function createTestFiles(searchResults: fileSearchResults, testFilePattern: RegE
                         throw new Error(`Found duplicate function ${functionName}`);
                     }
                     if (func && func.length > 0) {
-                        console.log(`found testcases(${testFile[func[0]].unitTests.length}) for ${functionName}`);
+                        console.log(`found testcases(${unitTest.length}) for ${functionName}`);
                         testFile[func[0]] = {
                             unitTests: unitTest,
                             v: filteredExport.fFiles[func[0]],
