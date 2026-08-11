2c9b6d1ab586d359b2f072f7e69547cbf600ac0d
MarcusJongste <jongsteml@gmail.com>
2026-08-11 22:15:07 +0200

    updated logger to show fullpath

--- src/output/logger.ts
+++ src/output/logger.ts
@@ -7,6 +7,7 @@ function logger(testResults: testResults): void {
             console.log(`Testcases(${failedTestResults.length}) failed for ${fullPath}`);
             failedTestResults.forEach(result => console.log(result.message));
         }
+        console.log(`ran tests for ${fullPath}`);
     });
     console.log(`Ran (${testResults.numberOfTestsRan}) | success:${testResults.numberOfSuccess} | failure:${testResults.numberOfFailure}`);
 }
