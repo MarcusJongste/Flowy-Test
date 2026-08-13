54f8c9675ed9d3de4a861864fd0db70083c526e7
MarcusJongste <jongsteml@gmail.com>
2026-08-13 13:15:30 +0200

    troubleshooting

--- dist/getTestData/sortModuleExportToTestFile.js
+++ dist/getTestData/sortModuleExportToTestFile.js
@@ -23,7 +23,7 @@ function createTestFiles(searchResults, testFilePattern) {
             return retDir;
         }, { testFiles: {}, fFiles: {} }))
             .then((filteredExport) => {
-            console.log('filteredExport size:', searchDir, filteredExport.testFiles.length);
+            console.log('filteredExport size:', searchDir, Object.keys(filteredExport.testFiles).length);
             return Object.entries(filteredExport.testFiles).reduce((testFile, [pathName, unitTest]) => {
                 const functionName = pathName.substring(pathName.lastIndexOf('\\') + 1), functionMatcher = new RegExp(`${functionName}$`), func = Object.keys(filteredExport.fFiles).filter(key => functionMatcher.test(key));
                 if (func.length > 1) {
